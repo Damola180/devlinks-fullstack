d536b79b36be8e8e4d1b3d75cb9ffa39d7b285c7
Damola180 <ojomuddav18@gmail.com>
2024-08-20 00:09:22 +0100

    test15

--- middleware.ts
+++ middleware.ts
@@ -5,20 +5,19 @@ const protectedRoutes = ["/application"];
 const unprotectedRoutes = ["/login", "/signup"];
 
 export default async function middleware(request: NextRequest) {
-  const session = await auth();
-
-  const isProtectedRoute = protectedRoutes.some((prefix) =>
-    request.nextUrl.pathname.startsWith(prefix)
-  );
-  const unisProtectedRoute = unprotectedRoutes.some((prefix) =>
-    request.nextUrl.pathname.startsWith(prefix)
-  );
-  if (!session && isProtectedRoute) {
-    const absoluteURL = new URL("/login", request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
-  if (session && unisProtectedRoute) {
-    const absoluteURL = new URL("/", request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
+  // const session = await auth();
+  // const isProtectedRoute = protectedRoutes.some((prefix) =>
+  //   request.nextUrl.pathname.startsWith(prefix)
+  // );
+  // const unisProtectedRoute = unprotectedRoutes.some((prefix) =>
+  //   request.nextUrl.pathname.startsWith(prefix)
+  // );
+  // if (!session && isProtectedRoute) {
+  //   const absoluteURL = new URL("/login", request.nextUrl.origin);
+  //   return NextResponse.redirect(absoluteURL.toString());
+  // }
+  // if (session && unisProtectedRoute) {
+  //   const absoluteURL = new URL("/", request.nextUrl.origin);
+  //   return NextResponse.redirect(absoluteURL.toString());
+  // }
 }
