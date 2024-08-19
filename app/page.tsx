d475455fec921cf8f4ac1c0a4ea5bc43f284f846
Damola180 <ojomuddav18@gmail.com>
2024-08-20 00:43:54 +0100

    test19

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-"use client";
 import { auth } from "@/auth";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
 
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session?.user) {
-      router.push("/application");
-    } else {
-      router.push("/login");
-    }
-  }, [session, router]);
   return (
     <>
       Link sharing App
