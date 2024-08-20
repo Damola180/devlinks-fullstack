a3978f3ffe1500cbdfd6b1560fbb41f66ee2b9b8
Damola180 <ojomuddav18@gmail.com>
2024-08-20 22:20:29 +0100

    test28

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 import { auth } from "@/auth";
 const protectedRoutes = ["/application"];
 const unprotectedRoutes = ["/login", "/signup"];
+const homeRoute = ["/", ""];
 
 export default async function middleware(request: NextRequest) {
   const session = await auth();
@@ -13,13 +14,16 @@ export default async function middleware(request: NextRequest) {
   const unisProtectedRoute = unprotectedRoutes.some((prefix) =>
     request.nextUrl.pathname.startsWith(prefix)
   );
+  const isHomeRoute = homeRoute.some(
+    (prefix) => request.nextUrl.pathname === prefix
+  );
 
   console.log(isProtectedRoute);
-  if (!session?.user?.email && isProtectedRoute) {
+  if (!session?.user?.email && (isProtectedRoute || isHomeRoute)) {
     const absoluteURL = new URL("/login", request.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
-  if (session?.user?.email && unisProtectedRoute) {
+  if (session?.user?.email && (unisProtectedRoute || isHomeRoute)) {
     const absoluteURL = new URL("/application", request.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
